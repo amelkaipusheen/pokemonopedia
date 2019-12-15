5a00a49f2a1a39eac359694b5e3a3bd982a50adc
mpietka <michalpietka11@gamil.com>
2019-12-15 20:49:51 +0100

    dodanie struktury i wygladu kart z pokemonami

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MatToolbarModule, MatCardModule } from "@angular/material";
+import { PokemonListComponent } from "./pokemon-list/pokemon-list.component";
+
+@NgModule({
+  declarations: [AppComponent, PokemonListComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatCardModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
